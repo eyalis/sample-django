0d0e26abda727ef2b5cb1555a45561f57ac2aefd
eyalis <eszewkis@yahoo.com>
2021-04-19 20:38:44 -0400

    fixed db

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,16 +92,15 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 if os.getenv("DEVELOPMENT_MODE", "True") == "True":
     DATABASES = {
         "default": {
-            # "ENGINE": "django.db.backends.sqlite3",
-            # "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-            "default": dj_database_url.parse(config('DATABASE_URL')),
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL",config('DATABASE_URL'))),
     }
 
 # Password validation
